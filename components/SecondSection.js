e28303fa772b5d0338ed67a9e3abb981017e7f02
ouardini <ouardini14@gmail.com>
2022-07-28 22:07:32 +0100

    Content

--- components/SecondSection.js
+++ components/SecondSection.js
@@ -1,31 +1,34 @@
 import React from "react";
-import Image from 'next/image'
+import Image from "next/image";
 export default function SecondSection() {
   return (
     <div className="py-8 w-screen  px-5 sm:px-6  lg:px-28  ">
       <div className=" space-y-7 md:space-y-0 md:grid md:grid-cols-3 items-center justify-center gap-10">
         <div className=" md:col-start-1 md:col-end-3">
-          <h2 className="font-AvenirHeavy text-slate-800 text-4xl md:text-6xl">Chi siamo?</h2>
-          <br/>
-          <p className="font-AvenirLT text-gray-600 text-xl md:text-2xl md:tracking-wider md:leading-9">
-            BDS è una società creata da professionisti di diversi settori che
-            hanno deciso offrire una nuova soluzione dal punto di vista
-            sanitario, spinti dalla direzione che stava prendendo il sistema
-            pubblico, inoltre col tempo si sono presentate altre opportunità da
-            inserire come il noleggio a lungo termine, oltre ad essere diventati
-            venditori professionisti di Amazon negli ultimi 2 anni.
+          <h2 className="font-AvenirHeavy text-slate-800 text-4xl md:text-6xl">
+            CHI SIAMO ?
+          </h2>
+          <br />
+          <p className="font-AvenirLT text-gray-600 text-xl md:text-xl md:tracking-wider md:leading-9">
+            La BDS è una società creata da professionisti di diversi settori,
+            nata dall{"'"}esigenza di trovare soluzioni per i propri clienti. <br/><br/>
+             Prima di tutte, vista la direzione in cui andava il sistema sanitario, si
+            è deciso di dare la possibilità ad ogni famiglia di avvicinarsi alla
+            sanità privata. <br/><br/>
+             Data la veloce crescita che sta avendo il mercato
+            delle vendite online, supportiamo le aziende che vogliono iniziare a
+            vendere prodotti su Amazon in maniera professionale. <br/><br/>
+             Un altro ramo di BDS si occupa di noleggio a medio e lungo termine per privati ed
+            aziende. <br/><br/>
+             Noi di Business Development & Strategy offriamo l{"'"}opportunità di rimanere al passo di un sistema in continua
+            evoluzione.
           </p>
         </div>
-           <div className="flex items-center justify-center">
-            <div className="relative w-[180px] h-[80px] md:w-[500px]  md:h-[180px]">
-            <Image
-            src={'/img/MainImage2.png'}
-            alt="logo"
-            layout="fill"
-            />
-            </div>
-            
-            </div>     
+        <div className="flex items-center justify-center">
+          <div className="relative w-[180px] h-[80px] md:w-[500px]  md:h-[180px]">
+            <Image src={"/img/MainImage2.png"} alt="logo" layout="fill" />
+          </div>
+        </div>
       </div>
     </div>
   );
--- components/ThirdSection.js
+++ components/ThirdSection.js
@@ -1,49 +1,79 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
 export default function ThirdSection() {
   return (
     <div className="py-8 w-screen space-y-8 px-5 sm:px-6  lg:px-28  ">
-        <h1 className='text-center font-AvenirHeavy tracking-tighter text-4xl'>I NOSTRI SERVIZI</h1>
-        <div className='flex flex-col items-center justify-center gap-8 md:grid md:grid-cols-3 '>
+      <h1 className="text-center font-AvenirHeavy tracking-tighter text-4xl">
+        I NOSTRI SERVIZI
+      </h1>
+      <div className="flex flex-col gap-5 2xl:gap-36 md:grid md:grid-cols-3 ">
+        <div className="flex flex-col  text-center space-y-3 ">
+          <div className=" md:min-h-[185px] flex  items-center justify-center ">
+            <Image
+              src={"/img/img1.png"}
+              alt="image1"
+              layout="intrinsic"
+              width={94}
+              height={94}
+            />
+          </div>
 
-            <div className='flex flex-col items-center justify-center text-center space-y-3'>
-                <Image
-                src={"/img/img1.png"}
-                alt="image1"
-                layout='intrinsic'
-                width={94}
-                height={94}
-                />
-                <h3 className='font-AvenirHeavy tracking-tighter text-3xl md:min-h-[80px] '>Sanità Privata</h3>
-                <p className='font-AvenirLT  '>Questo è un paragrafo. Fai clic qui per modificarlo e aggiungere il tuo testo</p>
-            </div>
-
-            <div className='flex flex-col items-center justify-center text-center space-y-3'>
-                <Image
-                src={"/img/img2.png"}
-                alt="image1"
-                layout='intrinsic'
-                width={80}
-                height={94}
-                />
-                <h3 className='font-AvenirHeavy tracking-tighter text-3xl md:min-h-[80px] '>Nollegio a lungo termine</h3>
-                <p className='font-AvenirLT  '>Questo è un paragrafo. Fai clic qui per modificarlo e aggiungere il tuo testo</p>
-            </div>
-
-            <div className='flex flex-col items-center justify-center text-center space-y-3'>
-                <Image
-                src={"/img/img3.png"}
-                alt="image1"
-                layout='intrinsic'
-                width={130}
-                height={94}
-                />
-                <h3 className='font-AvenirHeavy tracking-tighter text-3xl md:min-h-[80px] '>Vendita professionale su Amazone</h3>
-                <p className='font-AvenirLT  '>Questo è un paragrafo. Fai clic qui per modificarlo e aggiungere il tuo testo</p>
-            </div>
-
+          <h3 className="font-AvenirHeavy tracking-tighter text-3xl md:min-h-[85px] ">
+            Sanità Privata
+          </h3>
+          <p className="font-AvenirLT  text-[15px]">
+            Cosa ne pensi del sistema sanitario nazionale? <br/> <br/>
+            Nell{"'"}esigenza di una visita o una prestazione immediata ti rechi nel pubblico o nel
+            privato?  <br/> <br/>
+            Se ci fosse la soluzione adatta, con pochi soldi, per
+            mettere in protezione la tua famiglia e permetterti di scegliere
+            dove curarti la prenderesti in considerazione?  <br/> <br/>
+            Contattaci per saperne di più
+          </p>
         </div>
 
+        <div className="flex flex-col  text-center space-y-3  ">
+          <div className=" md:min-h-[185px] flex  items-center justify-center ">
+            <Image
+              src={"/img/img2.png"}
+              alt="image1"
+              layout="intrinsic"
+              width={263}
+              height={171}
+            />
+          </div>
+
+          <h3 className="font-AvenirHeavy tracking-tighter text-3xl  md:min-h-[85px] ">
+            Nollegio a lungo termine
+          </h3>
+          <p className="font-AvenirLT text-[15px] ">
+          La nostra expertise al servizio della tua flotta aziendale con soluzioni di noleggio a medio e lungo termine, dotate di strumenti innovativi per soddisfare qualsiasi tua esigenza.
+
+          </p>
+        </div>
+
+        <div className="flex flex-col  text-center space-y-3 ">
+          <div className=" md:min-h-[185px] flex  items-center justify-center ">
+            <Image
+              src={"/img/img3.png"}
+              alt="image1"
+              layout="intrinsic"
+              width={170}
+              height={71}
+            />
+          </div>
+
+          <h3 className="font-AvenirHeavy tracking-tighter text-3xl  md:min-h-[85px]">
+            Vendita professionale su Amazone
+          </h3>
+          <p className="font-AvenirLT  text-[15px]">
+          Hai un{"'"}azienda e attualmente stai pensando o hai già iniziato a vendere online? <br/>
+          Vorresti valutare la possibilità di utilizzare Amazon come canale di vendita? <br/> <br/>
+          Iniziare a vendere su questo marketplace da soli non è semplice, rischi solo di perdere tempo e non ottenere risultati.<br/> <br/>
+          Noi di BDS abbiamo un gruppo di professionisti che possono aiutare la tua azienda a portare i propri prodotti su Amazon affiancandoti per raggiungere col tempo grandi risultati.
+          </p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
