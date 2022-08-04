b2bc9b5c2ea1341d12f711a5a331f0c4b8a727f8
ouardini <ouardini14@gmail.com>
2022-08-04 16:32:54 +0100

    Cookies

--- components/Cookies.js
+++ components/Cookies.js
@@ -13,7 +13,7 @@ const [cookiesWarn,setCookiesWarn]=useState(true)
   return ( 
     <div className={`origin-bottom fixed bottom-0   space-y-4  font-AvenirLT  w-screen     bg-slate-800 py-5 px-9 z-20 ${cookiesWarn?'inline':'hidden'}`}>
           <p className='text-white '>
-          I cookie ci aiutano a fornire un servizio migliore, "Business Sviluppo & "Strategia" utilizza i cookie per eseguire analisi e fornire contenuti e annunci su misura per i tuoi interessi. Visitando il nostro sito Web accetti il ​​nostro utilizzo dei cookie.
+          I cookie ci aiutano a fornire un servizio migliore, `Business Sviluppo & Strategia `utilizza i cookie per eseguire analisi e fornire contenuti e annunci su misura per i tuoi interessi. Visitando il nostro sito Web accetti il nostro utilizzo dei cookie.
            </p>
         <div>
         <button className='  rounded-md bg-white px-3 py-1' onClick={()=>{enableCookies()}}> 
